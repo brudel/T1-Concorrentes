be299b24313258051676b68a87c730f83023ab46
brudel <brudel13742@gmail.com>
2019-11-05 01:13:36 -0300

    Alternativa

--- alternativa.c
+++ alternativa.c
@@ -7,137 +7,22 @@
 
 #define swap(i, j) aux = i, i = j, j = aux;
 
-int max(int* vector, int n){
-	int m = vector[0];
-	for(int i=1;i<n;i++){
-		if(vector[i] > m){
-			m = vector[i];
-		}
-	}
-	return m;
+typedef int estogram[100];
+
+int A;
+
+float mediana(estogram est) {
+	int sum = -1, i;
+
+	for (i = 0; sum < A / 2; ++i)
+		sum += est[i];
+
+	return i;
 }
 
-int min(int* vector, int n){
-	int m = vector[0];
-	for(int i=1;i<n;i++){
-		if(vector[i] < m){
-			m = vector[i];
-		}
-	}
-	return m;
-}
-
-int quickoddmedian(int* vet, int high, int target) {
-	int pivo, aux, i = 0, rnd = rand() % high;
-	swap(vet[0], vet[rnd]);
-	pivo = vet[0];
-
-	for (int j = 1; j < high; j++)
-		if (vet[j] < pivo) {
-			++i;
-			swap(vet[i], vet[j]);
-		}
-
-	swap(vet[0], vet[i]);
-	if (target == i)
-		return vet[i];
-
-	if (target < i)
-		return quickoddmedian(vet, i, target);
-	else
-		return quickoddmedian(vet + i + 1, high - i - 1, target - i - 1);
-}
-
-float quickevenmedian(int* vet, int high, int target) {
-	int pivo, aux, i = 0, rnd = rand() % high;
-	swap(vet[0], vet[rnd]);
-	pivo = vet[0];
-
-	for (int j = 1; j < high; j++)
-		if (vet[j] < pivo) {
-			++i;
-			swap(vet[i], vet[j]);
-		}
-
-	swap(vet[0], vet[i]);
-	if (target == i)
-		return (float) (vet[i] + max(vet, i)) / 2;
-
-	if (target == i + 1)
-		return (float) (vet[i] + min(vet + i + 1, high - i - 1)) / 2;
-
-	if (target < i)
-		return quickevenmedian(vet, i, target);
-	else
-		return quickevenmedian(vet + i + 1, high - i - 1, target - i - 1);
-}
-
-void counting_sort(int *vector, int n) {
-	int i, min = INT_MAX, max = INT_MIN;
-	int *Aux, *R;
-
-	// Encontrar a chave mínima e a máxima
-	for (i = 0; i < n; i++) {			// c1 * n
-		if (vector[i] > max) max = vector[i];
-		if (vector[i] < min) min = vector[i];
-	}
-
-	// Declarar o vetor auxiliar (max-min+1)
-	Aux = (int *) calloc(max-min+1, sizeof(int));   // c2 * (max-min+1)
-
-	// Contar a partir do vetor original, quantas vezes cada chave ocorre
-	for (i = 0; i < n; i++) Aux[vector[i]-min]++;  // c3 * n
-
-	// Função cumulativa
-	for (i = 1; i < max-min+1; i++) Aux[i] += Aux[i-1];   // c4 * (max-min)
-
-	// Ordenação ocorre aqui!
-	R = (int *) malloc(sizeof(int) * n);
-
-	for (i = n-1; i >= 0; i--) R[--Aux[vector[i]-min]] = vector[i]; // c5 * n
-	for (i = 0; i < n; i++) vector[i] = R[i];		// c6 * n
-
-	free(Aux);
-	free(R);
-}
-
-// Ordena lin linhas passadas (ou seja, ordena cidades, regioes e o brasil dependendo do valor de col)
-void ordena_linhas(int *matriz, int lin, int col)
-{
-	int j;
-
-	for (j = 0; j < lin; j++)
-	{
-		counting_sort(&matriz[j*col], col);
-	}
-}
-
-void encontra_maiores(int *matriz, int *maiores, int lin, int col){
-	for(int i=0; i<lin; i++){
-		maiores[i] = matriz[col*i + col-1];
-	}
-}
-
-void encontra_menores(int *matriz, int *menores, int lin, int col){
-	for(int i=0; i<lin; i++){
-		menores[i] = matriz[col*i];
-	}
-}
-
-// Mediana de cada linha (cidade, região ou o brasil, dependendo do valor de col)
-void calcula_mediana(int *matriz, float *vet, int lin, int col)
-{
-	int j;
-
-	for (j = 0; j < lin; j++)
-	{
-		vet[j] = matriz[col*j + col/2];
-		if(!(col % 2))
-		{
-			vet[j] += matriz[col*j + col/2-1];
-			vet[j] = (float )vet[j]*0.5;
-		}
-	}
+void sum_est(estogram A, estogram B) {
+	for (int i = 0; i < 100; ++i)
+		A[i] += B[i];
 }
 
 int main(void)
@@ -154,21 +39,24 @@ int main(void)
 	//	 exit(1);
 	// }
 
-	while(scanf("%d", &lines[i]) != EOF)
+	while(scanf(" %d", &lines[i]) != EOF)
 		i++;
 
 	int R = lines[0];
 	int C = lines[1];
-	int A = lines[2];
+	A = lines[2];
 	int seed = lines[3];
 
 	/// Generates the matrix
 	srand(seed);
-	int *matriz = (int *)malloc(R*C*A*sizeof(int));
-	for (i = 0; i < R*C*A; i++)
-	{
-		matriz[i] = rand() % 100;
-	}
+	estogram *matriz = calloc(1, R * C * sizeof(estogram)),
+	*regioes = calloc(1, R * sizeof(estogram)),
+	*Brasil = calloc(1, sizeof(estogram));
+	for (i = 0; i < R*C; i++)
+		for (j = 0; j < A; j++)
+			++matriz[i][rand() % 100];
+
+
 
 	// Print array in matrix's representation	
 	// for (i = 0; i < R*C; i++)
@@ -180,25 +68,25 @@ int main(void)
    
 	/// Calculate the metrics
 	/// Mean and deviation
-	float *media_cidades = (float *) malloc(R*C*sizeof(float));
-	float *media_regiao = (float *) malloc(R*sizeof(float));
+	float *media_cidades = (float *) calloc(1, R*C*sizeof(float));
+	float *media_regiao = (float *) calloc(1, R*sizeof(float));
 	float media_brasil;
 
-	float *dp_cidades = (float *) malloc(R*C*sizeof(float));
-	float *dp_regiao = (float *) malloc(R*sizeof(float));
+	float *dp_cidades = (float *) calloc(1, R*C*sizeof(float));
+	float *dp_regiao = (float *) calloc(1, R*sizeof(float));
 	float dp_brasil;
 
 	/// Median, max and min
-	float *mediana_cidades = (float *) malloc(R*C*sizeof(float));
-	float *mediana_regiao = (float *) malloc(R*sizeof(float));
+	float *mediana_cidades = (float *) calloc(1, R*C*sizeof(float));
+	float *mediana_regiao = (float *) calloc(1, R*sizeof(float));
 	float mediana_brasil;
 
-	int *maior_cidades = (int *) malloc(R*C*sizeof(int));
-	int *maior_regiao = (int *) malloc(R*sizeof(int));
+	int *maior_cidades = (int *) calloc(1, R*C*sizeof(int));
+	int *maior_regiao = (int *) calloc(1, R*sizeof(int));
 	int maior_brasil = -1;
 
-	int *menor_cidades = (int *) malloc(R*C*sizeof(int));
-	int *menor_regiao = (int *) malloc(R*sizeof(int));
+	int *menor_cidades = (int *) calloc(1, R*C*sizeof(int));
+	int *menor_regiao = (int *) calloc(1, R*sizeof(int));
 	int menor_brasil = INT_MAX;
 
 	// Calcula media, desvio padrão, max e min das cidades, regioes e país, propagando as somas parciais
@@ -212,7 +100,7 @@ int main(void)
 	int elem, maiorC = -1, maiorR = -1, menorC = INT_MAX, menorR = INT_MAX;
 	tempoExec = clock();
 	nclocksMedia = clock();
-	for(int i=0; i<R; i++){
+	/*for(int i=0; i<R; i++){
 		s3 = 0;
 		s4 = 0;
 		maiorR = -1;
@@ -298,27 +186,15 @@ int main(void)
 	// calcula media e dp do país
 	media_brasil = s5/(R*C*A);
 	dp_brasil = sqrt((s6  -  (float) s5 * s5 / (R*C*A))	/	(R*C*A - 1));
-	nclocksMedia = clock() - nclocksMedia;
+	nclocksMedia = clock() - nclocksMedia; */
 
 	// Mediana da cidade
 	nclocksSortingC = clock();
-	 ordena_linhas(matriz, R*C, A);
-	// encontra_maiores(matriz, maior_cidades, R*C, A);
-	// encontra_menores(matriz, menor_cidades, R*C, A);
-	 calcula_mediana(matriz, mediana_cidades, R*C, A);
-	/*if(!(A%2)){
-		for(int i=0; i<R; i++){
-			for(int j=0; j<C; j++){
-				mediana_cidades[i*C + j] = quickevenmedian(&matriz[i*C*A + j*A], A, A/2);
-			}
+	for(int i=0; i<R; i++)
+		for(int j=0; j<C; j++){
+			mediana_cidades[i*C + j] = mediana(matriz[i*C + j]);
+			sum_est(regioes[i], matriz[i*C + j]);
 		}
-	} else {
-		for(int i=0; i<R; i++){
-			for(int j=0; j<C; j++){
-				mediana_cidades[i*C + j] = quickoddmedian(&matriz[i*C*A + j*A], A, A/2);
-			}
-		}
-	}*/
 	
 	nclocksSortingC = clock() - nclocksSortingC;
 
@@ -332,10 +208,16 @@ int main(void)
 
 	// Mediana da regiao
 	nclocksSortingR = clock();
-	ordena_linhas(matriz, R, A*C);
+	//ordena_linhas(matriz, R, A*C);
 	// encontra_maiores(matriz, maior_regiao, R, A*C);
 	// encontra_menores(matriz, menor_regiao, R, A*C);
-	calcula_mediana(matriz, mediana_regiao, R, A*C);
+	//calcula_mediana(matriz, mediana_regiao, R, A*C);
+
+
+	for(int i=0; i<R; i++)	{
+		mediana_regiao[i] = mediana(regioes[i]);
+		sum_est(*Brasil, regioes[i]);
+	}
 	nclocksSortingR = clock() - nclocksSortingR;
 
 	// printf("\n");
@@ -348,14 +230,15 @@ int main(void)
 
 	// Mediana do Brasil
 	nclocksSortingP = clock();
-	ordena_linhas(matriz, 1, A*C*R);
+	//ordena_linhas(matriz, 1, A*C*R);
 	// encontra_maiores(matriz, &maior_brasil, 1, A*C*R);
 	// encontra_menores(matriz, &menor_brasil, 1, A*C*R);
-	calcula_mediana(matriz, &mediana_brasil, 1, A*C*R);
+	//calcula_mediana(matriz, &mediana_brasil, 1, A*C*R);
+	mediana_brasil = mediana(*Brasil);
 	nclocksSortingP = clock() - nclocksSortingP;
 
 	tempoExec = (clock() - tempoExec)/CLOCKS_PER_SEC;
-
+	/*
 	// printf("\n");
 	// for (i = 0; i < R*C; i++)
 	// {
@@ -384,7 +267,7 @@ int main(void)
 		printf("Reg %d: menor: %d, maior: %d, mediana: %.2f, media: %.2f e DP: %.2f\n", i, menor_regiao[i], maior_regiao[i], mediana_regiao[i], media_regiao[i], dp_regiao[i]);
 	}
 
-	printf("\n");
+	printf("\n");*/
 
 	// Metrics for the country
 	printf("Brasil: menor: %d, maior: %d, mediana: %.2f, media: %.2f e DP: %.2f\n", menor_brasil, maior_brasil, mediana_brasil, media_brasil, dp_brasil);
